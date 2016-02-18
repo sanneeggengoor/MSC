cee9493199e521fb42b7f5ffe65f9ee3bf200562
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-18 17:43:13 +0100

    Merge branch 'master' of https://github.com/sanneeggengoor/MSC
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,35 @@ package com.company;
 
 public class Main {
 
+    private static int[] genome;
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
-        System.out.println("Bye World!");
-        System.out.println("Hoi vrienden");
-        System.out.println("Werkt dit?");
-        System.out.println("ja geweldig");
+        genome = createGenome();
+        inverse(4,8);
+        for(int i = 0; i<25; i++){
+            System.out.println(genome[i]);
+        }
+
+    }
+
+    private static int[] createGenome(){
+        int[] genome = {
+                23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20,
+                5, 8, 18, 12, 13, 14, 15, 16, 17,
+                21, 3, 4, 9
+        };
+        return genome;
+    }
+
+    private static void invert(int a, int b){
+        int[] inverseGen= new int[25];
+        for(int i = a; i<=b; i++){
+            
+        }
+        System.out.println
+        for(int i = 0; i < 25; i++){
+            if(a<i && i<b){
+                genome[i]=inverseGen[i];
+            }
+        }
     }
 }
