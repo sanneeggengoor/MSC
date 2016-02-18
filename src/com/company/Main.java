a389c5740a0b09200de71dfe3c50a147ec5643b8
sanneeggengoor <sanneeggengoor@gmail.com>
2016-02-18 17:53:05 +0100

    inversie werkt

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,32 +5,26 @@ public class Main {
     private static int[] genome;
     public static void main(String[] args) {
         genome = createGenome();
-        inverse(4,8);
-        for(int i = 0; i<25; i++){
-            System.out.println(genome[i]);
-        }
-
+        invert(4,8);
     }
 
     private static int[] createGenome(){
-        int[] genome = {
+        int[] genomeCreate = {
                 23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20,
                 5, 8, 18, 12, 13, 14, 15, 16, 17,
                 21, 3, 4, 9
         };
-        return genome;
+        return genomeCreate;
     }
 
     private static void invert(int a, int b){
         int[] inverseGen= new int[25];
         for(int i = a; i<=b; i++){
-
+            int x = b - i + a;
+            inverseGen[i-1] = genome[x-1];
         }
-        System.out.println
-        for(int i = 0; i < 25; i++){
-            if(a<i && i<b){
-                genome[i]=inverseGen[i];
-            }
+        for(int i = a; i <= b; i++){
+            genome[i-1]=inverseGen[i-1];
         }
     }
 }
