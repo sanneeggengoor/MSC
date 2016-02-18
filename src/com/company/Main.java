e387c9e8c2b5f78b5a5be7383ba9f9b5017bf520
MaaikeVollebergh <maaike@vollebergh.nl>
2016-02-18 18:15:36 +0100

    Checken

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,13 @@ package com.company;
 public class Main {
 
     private static int[] genome;
+
     public static void main(String[] args) {
         genome = createGenome();
-        invert(4,8);
+        invert(4, 8);
     }
 
-    private static int[] createGenome(){
+    private static int[] createGenome() {
         int[] genomeCreate = {
                 23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20,
                 5, 8, 18, 12, 13, 14, 15, 16, 17,
@@ -17,14 +18,23 @@ public class Main {
         return genomeCreate;
     }
 
-    private static void invert(int a, int b){
-        int[] inverseGen= new int[25];
-        for(int i = a; i<=b; i++){
+    private static void invert(int a, int b) {
+        int[] inverseGen = new int[25];
+        for (int i = a; i <= b; i++) {
             int x = b - i + a;
-            inverseGen[i-1] = genome[x-1];
+            inverseGen[i - 1] = genome[x - 1];
         }
-        for(int i = a; i <= b; i++){
-            genome[i-1]=inverseGen[i-1];
+        for (int i = a; i <= b; i++) {
+            genome[i - 1] = inverseGen[i - 1];
+        }
+    }
+
+    private static void checkFront(int m) {
+        for (int i = 0; i < 25; i++) {
+            if (genome[i] == m) {
+                invert(m, i + 1);
+                break;
+            }
         }
     }
 }
