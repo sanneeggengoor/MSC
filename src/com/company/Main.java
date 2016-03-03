b82dc6e4210cbbb857e4097c7f3d2ce2baa46af5
sanneeggengoor <sanneeggengoor@gmail.com>
2016-03-03 19:17:59 +0100

    geordend

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,14 +20,7 @@ public class Main {
  */
     }
 
-    private static int[] createGenome(){
-        int[] genomeCreate = {
-                23, 1, 2, 11, 24, 22, 19, 6, 10, 7, 25, 20,
-                5, 8, 18, 12, 13, 14, 15, 16, 17,
-                21, 3, 4, 9
-        };
-        return genomeCreate;
-    }
+   
 
 /**
     private static void checkFrontAndBack() {
