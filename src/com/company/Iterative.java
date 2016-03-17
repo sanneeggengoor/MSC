9f4ae97004df75e0a72d0f4af44adb7ac47f8329
sanneeggengoor <sanneeggengoor@gmail.com>
2016-03-17 17:24:28 +0100

    Merge branch 'master' of https://github.com/sanneeggengoor/MSC
    # Conflicts:
    #	src/com/company/Main.java

---
+++ src/com/company/Iterative.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+/**
+ * Created by Sanne on 17-3-2016.
+ */
+public class Iterative {
+    Stack<Genome> deepening;
+    HashSet<String> listAll;
+    
+}
