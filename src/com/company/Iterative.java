e21d88c2691adcc23822b82974b54ccef1712521
sanneeggengoor <sanneeggengoor@gmail.com>
2016-03-17 17:35:32 +0100

    Merge branch 'master' of https://github.com/sanneeggengoor/MSC
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Iterative.java
+++ src/com/company/Iterative.java
@@ -9,5 +9,28 @@ import java.util.Stack;
 public class Iterative {
     Stack<Genome> deepening;
     HashSet<String> listAll;
-    
+    int maxDepth;
+
+    public Iterative() {
+        deepening = new Stack<>();
+        listAll = new HashSet<>();
+        maxDepth = 1;
+    }
+
+    public void createChildrenStack(){
+        Genome parent = deepening.pop();
+
+        for(int i = 0; i<25; i++)
+            for(int j = i ; j < 25; j++){
+                Genome child = parent.invert(i,j);
+                if (!listAll.contains(child)) {
+                    child.count = parent.count + 1;
+                    deepening.push(child);
+                    listAll.add(child.toString());
+                }
+            }
+    }
+
+
+}
 }
