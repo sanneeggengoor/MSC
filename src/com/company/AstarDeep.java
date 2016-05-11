54aa6d51bf06247ed367879cf0cc7c08cf8f2377
sanneeggengoor <sanneeggengoor@gmail.com>
2016-05-11 09:14:04 +0200

    Astar gemaakt en onnodige inverts weggehaald

--- src/com/company/AstarDeep.java
+++ src/com/company/AstarDeep.java
@@ -29,7 +29,7 @@ public class AstarDeep {
         genomePrior.add(gen);
         solutionFound = false;
         while(!solutionFound) {
-            while (!solutionFound && countParents < 40000) {
+            while (!solutionFound && countParents < 10000) {
                 createChildrenPrior();
                 countParents++;
                 if (countParents % 200 == 0) {
